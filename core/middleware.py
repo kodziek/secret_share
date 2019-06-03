0e15104da40c9fe4f861f053deba944068f55aec
Konrad L. Kodziek <kodziek@kodziek.pl>
2019-06-03 19:32:58 +0200

    Add middleware for saving last user agent

---
+++ core/middleware.py
@@ -0,0 +1,12 @@
+class UserAgentMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user
+        user_agent = request.headers['User-Agent']
+        if user.last_user_agent != user_agent:
+            user.last_user_agent = user_agent
+            user.save()
+
+        return self.get_response(request)
--- secret_share/settings.py
+++ secret_share/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.UserAgentMiddleware',
 ]
 
 ROOT_URLCONF = 'secret_share.urls'
