5a11c7353e067b107ac52f1bbeecc6f18f1704e9
Konrad L. Kodziek <kodziek@kodziek.pl>
2019-06-04 19:05:04 +0200

    Restrict items access to created during last 24 hours

--- items/models.py
+++ items/models.py
@@ -1,6 +1,16 @@
 import uuid
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
 from django.conf import settings
 from django.db import models
+
+
+class ItemManager(models.QuerySet):
+
+    def filter(self, *args, **kwargs):
+        yesterday = datetime.now() - relativedelta(days=1)
+        return super().filter(create_date__gte=yesterday, *args, **kwargs)
 
 
 class Item(models.Model):
@@ -12,6 +22,8 @@ class Item(models.Model):
     password = models.CharField(max_length=128)
     url = models.TextField(null=True, blank=True)
     file = models.FileField(null=True, blank=True)
+
+    objects = ItemManager.as_manager()
 
     def __str__(self):
         return self.url or str(self.file)
