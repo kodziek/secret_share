a02f47ee6e15d9c2a2b362d35a0e52d3a58a33d3
Konrad L. Kodziek <kodziek@kodziek.pl>
2019-06-10 18:31:09 +0200

    Add missing tests for models

--- items/tests/test_models.py
+++ items/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from freezegun import freeze_time
 
@@ -14,15 +15,17 @@ class ItemModelTestCase(TestCase):
     def setUpTestData(cls):
         user = UserFactory()
         with freeze_time(cls.yesterday):
-            cls._create_item(user)
+            cls.url_item = cls._create_item(user, url='http://kodziek.pl')
         with freeze_time(cls.almost_yesterday):
-            cls._create_item(user)
+            cls.file_item = cls.file_url = cls._create_item(
+                user, file=SimpleUploadedFile('file', b'content'),
+            )
         with freeze_time(cls.today):
             cls._create_item(user)
 
     @classmethod
-    def _create_item(cls, user):
-        ItemFactory(password='a', user=user)
+    def _create_item(cls, user, **kwargs):
+        return ItemFactory(password='a', user=user, **kwargs)
 
     @freeze_time(today)
     def test_default_manager_returns_only_today_item(self):
@@ -31,3 +34,9 @@ class ItemModelTestCase(TestCase):
     @freeze_time(today)
     def test_all_objects_manager_returns_only_today_item(self):
         self.assertEqual(Item.all_objects.all().count(), 3)
+
+    def test_str_for_item_with_url(self):
+        self.assertEqual(str(self.url_item), self.url_item.url)
+
+    def test_str_for_item_with_fiel(self):
+        self.assertEqual(str(self.file_item), self.file_item.file)
